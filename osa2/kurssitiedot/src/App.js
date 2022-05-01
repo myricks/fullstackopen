d4282b3b582712380883e1ee62f481d5df316706
joonas <joonas.ylisaari@gmail.com>
2022-05-01 11:47:41 +0300

    2.4 ja 2.5: kurssitiedot ja erillinen moduuli. Tein aluksi coursesta oman moduulin

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,54 +1,58 @@
 import Course from './components/Course';
 
 
-const Total = ({ parts }) => {
-  const total = parts.reduce(
-    (previousValue, parts) => previousValue + parts.exercises, 0);
-  return (
-    <div>
-      <h4>
-        Total of {total} exercises
-      </h4>
-    </div>
-  )
-}
-
 const App = () => {
-  const course =
-  {
-    name: 'Half stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of react',
-        exercises: 10,
-        id: 1
-      },
-
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
 
   return (
     <div>
-      <Course course={course} />
-      <Total parts={course.parts} />
+      {courses.map((course) =>
+        <Course key={course.id} course={course} />
+      )}
     </div>
   );
 }
