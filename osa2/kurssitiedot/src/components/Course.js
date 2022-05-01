2e52003c8e69ac04f567fa65b7a94473658015e3
joonas <joonas.ylisaari@gmail.com>
2022-05-01 11:48:17 +0300

    2.4 liittyen total siiretty courseen

--- osa2/kurssitiedot/src/components/Course.js
+++ osa2/kurssitiedot/src/components/Course.js
@@ -26,11 +26,24 @@ const Content = ({ parts }) => {
     );
 }
 
+const Total = ({ parts }) => {
+    const total = parts.reduce(
+        (previousValue, parts) => previousValue + parts.exercises, 0);
+    return (
+        <div>
+            <h4>
+                Total of {total} exercises
+            </h4>
+        </div>
+    )
+}
+
 const Course = ({ course }) => {
     return (
         <div>
             <Header header={course.name} />
             <Content parts={course.parts} />
+            <Total parts={course.parts} />
         </div>
     );
 }
